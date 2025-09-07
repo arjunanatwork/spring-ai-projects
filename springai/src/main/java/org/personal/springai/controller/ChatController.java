38d18d2035eb9755f70fcfaae0bd4e262ab51f4c
Arjunan Ravi <arjunanatwork@gmail.com>
2025-09-07 18:31:46 +0400

    Adding a config class

--- springai/src/main/java/org/personal/springai/controller/ChatController.java
+++ springai/src/main/java/org/personal/springai/controller/ChatController.java
@@ -12,22 +12,14 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     @GetMapping("/chat")
     public String chat(@RequestParam(required = false) String message) {
         return chatClient
                 .prompt()
-                .system("""
-                         You are an internal IT helpdesk assistant. Your role is to assist\s
-                        employees with IT-related issues such as resetting passwords,\s
-                        unlocking accounts, and answering questions related to IT policies.
-                        If a user requests help with anything outside of these\s
-                        responsibilities, respond politely and inform them that you are\s
-                        only able to assist with IT support tasks within your defined scope.
-                        """)
                 .user(message)
                 .call().content();
     }
