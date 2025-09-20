c3fec2a294d1d4afd4ea8874a01db8f5990f932f
Arjunan Ravi <arjunanatwork@gmail.com>
2025-09-20 22:40:56 +0400

    Add new endpoints for structured output in chat responses
    Introduce three new endpoints: /chat-list, /chat-map, and /chat-bean-list. These endpoints handle lists, maps, and typed bean lists, utilizing custom converters for structured AI responses.

--- springai/src/main/java/org/personal/springai/controller/StructuredOutputController.java
+++ springai/src/main/java/org/personal/springai/controller/StructuredOutputController.java
@@ -3,11 +3,18 @@ package org.personal.springai.controller;
 import org.personal.springai.model.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +32,33 @@ public class StructuredOutputController {
                 .prompt()
                 .user(message)
                 .call().entity(CountryCities.class);
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-list")
+    public ResponseEntity<List<String>> chatList(@RequestParam(required = false) String message) {
+        List<String> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ListOutputConverter());
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-map")
+    public ResponseEntity<Map<String, Object>> chatMap(@RequestParam(required = false) String message) {
+        Map<String, Object> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new MapOutputConverter());
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(@RequestParam(required = false) String message) {
+        List<CountryCities> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ParameterizedTypeReference<List<CountryCities>>() {});
         return ResponseEntity.ok(countryCities);
     }
 
