68f03d8bb618ffe1f519e9fbad19762ff7d4149c
Arjunan Ravi <arjunanatwork@gmail.com>
2025-09-30 05:26:35 +0400

    Add ChatMemoryChatClientConfig and ChatMemoryChatController
    Introduced a configuration class to define a ChatClient bean and a controller to handle chat-related API requests. The controller integrates with the ChatClient to process user messages and return responses.

---
+++ springai/src/main/java/org/personal/springai/config/ChatMemoryChatClientConfig.java
@@ -0,0 +1,14 @@
+package org.personal.springai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatMemoryChatClientConfig {
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.build();
+    }
+}
