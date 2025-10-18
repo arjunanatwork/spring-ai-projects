5a01c4470a8d0628b011996bbcd02c6e271f76b3
Arjunan Ravi <arjunanatwork@gmail.com>
2025-10-18 20:32:15 +0400

    Add HelpDesk tools, controller, and configuration updates
    Introduce HelpDeskTools for ticket operations and integrate it with a new HelpDeskController to handle API interactions. Update configuration to use a system prompt template and adjust default advisors to enhance chat client capabilities. Enable H2 console for easier database debugging.

--- springai/src/main/java/org/personal/springai/config/HelpDeskChatClientConfig.java
+++ springai/src/main/java/org/personal/springai/config/HelpDeskChatClientConfig.java
@@ -7,22 +7,28 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import java.util.List;
 
 @Configuration
 public class HelpDeskChatClientConfig {
 
+    @Value("classpath:/promptTemplates/helpDeskSystemPromptTemplate.st")
+    Resource systemPromptTemplate;
+
     @Bean("helpDeskChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, TimeTools timeTools) {
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
         Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
-        MessageChatMemoryAdvisor chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
+                .defaultSystem(systemPromptTemplate)
                 .defaultTools(timeTools)
-                .defaultAdvisors(List.of(loggerAdvisor, chatMemoryAdvisor, tokenUsageAdvisor))
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor,tokenUsageAdvisor))
                 .build();
     }
 }
